93dab5c25ca60e11350caf2db8b1c432a0ec8c8e
rizkysp6162 <rizkysp6162@gmail.com>
2023-05-04 22:14:20 +0700

    View About
    +nambah link map tok hehe

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,11 +9,12 @@
   <!-- Deskripsi -->
   
   
-  <div class='desc'>
+  <div class='adesc'>
     <img class='desc1' src="/img/desc.png" alt="Gambar Toko">
-    <div class="desc2">
-      <div class='desc2a'>House of Pet Merupakan salah satu petshop terlengkap yang ada di daerah Malang, tepatnya di Ruko Bisnis Center, Jl. Soekarno Hatta No. Kav. 22, Jatimulyo, Kec. Lowokwaru, Kota Malang, Jawa Timur 65142.</div>
-      <div class='desc2a'>House of Pet memberikan pelayanan yang terbaik untuk Anda, dengan pekerja yang profesional dan yang pastinya berpengalaman di bidangnya masing-masing. </div>
+    <div class="adesc2">
+      <div class='adesc2a'>House of Pet Merupakan salah satu petshop terlengkap yang ada di daerah Malang, tepatnya di Ruko Bisnis Center, Jl. Soekarno Hatta No. Kav. 22, Jatimulyo, Kec. Lowokwaru, Kota Malang, Jawa Timur 65142.</div>
+      <a class="linkmap" href="https://goo.gl/maps/XxyK5DJxkZHFGstR6?coh=178572&entry=tt">Go to maps</a>
+      <div class='adesc2a'>House of Pet memberikan pelayanan yang terbaik untuk Anda, dengan pekerja yang profesional dan yang pastinya berpengalaman di bidangnya masing-masing. </div>
     </div>
   </div>
 
