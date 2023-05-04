ded47e968056682e068d7aa2d99bf5c046133ea3
rizkysp6162 <rizkysp6162@gmail.com>
2023-05-04 11:44:27 +0700

    nambah hello worlrd
    nde dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -27,6 +27,7 @@
 <!-- Selamat Datang -->
 <div class='seldat'>
   <div class='seldat1'>Selamat Datang Di</div>
+  <p>hello world</p>
   <div class='seldat2'>HOUSE OF PET</div>
 </div>
 <!-- Deskripsi -->
